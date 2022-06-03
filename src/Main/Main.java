c9555bcd2055f5961040540a32035b663daded1e
Juan Isturiz <juan.isturiz@correo.unimet.edu.ve>
2022-06-02 23:24:00 -0400

    Assembler y main modificados para que estén chikiluky

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,6 +10,8 @@ import Primitivas.ProduBotones;
 import Primitivas.ProduCamaras;
 import Primitivas.ProduPantallas;
 import Primitivas.ProduPin;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -22,19 +24,32 @@ public class Main {
     public static volatile int cams = 0;
     public static volatile int screens = 0;
     public static volatile int pins = 0;
-    
+
     public static volatile int totalPhones = 0;
     public static volatile int totalDelivery = 0;
-    
+
+    public static float cdi;
+    public static float pantallas;
+    public static float botones;
+    public static float pines;
+    public static float camaras;
+    public static float needPantalla;
+    public static float needPines;
+    public static float needCamaras;
+    public static float price;
+    public static float maxProd;
+
     public static Semaphore mutexButt;
     public static Semaphore mutexCams;
     public static Semaphore mutexScreens;
     public static Semaphore mutexPins;
     public static Semaphore mutexDays;
+
     public static Semaphore mutexPhones;
     public static Semaphore mutexTotalDelivery;
+
     public static Semaphore playBoss;
-    
+
     public static Semaphore semProdCams;
     public static Semaphore semConsCams;
     public static Semaphore semProdButt;
@@ -43,38 +58,75 @@ public class Main {
     public static Semaphore semConsPins;
     public static Semaphore semProdScreens;
     public static Semaphore semConsScreens;
-    
+
     public static volatile ProduBotones amtProdbutt[];
     public static volatile ProduCamaras amtProdcam[];
     public static volatile ProduPantallas amtProdscreen[];
     public static volatile ProduPin amtProdpin[];
     public static volatile Assembler amtAssembler[];
-    
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Map<String, float[]> map = new HashMap<String, float[]>();
+
+        float[] juan = new float[9];
+
+        juan[0] = 1;
+        juan[1] = 1;
+        juan[2] = 4;
+        juan[3] = (float) 0.5;
+        juan[4] = (float) 0.5;
+        juan[5] = 3;
+        juan[6] = 1;
+        juan[7] = 4;
+        juan[8] = 900;
+
+        float[] vic = new float[9];
+
+        vic[0] = 4;
+        vic[1] = 2;
+        vic[2] = 2;
+        vic[3] = (float) 0.33;
+        vic[4] = (float) 0.5;
+        vic[5] = 2;
+        vic[6] = 1;
+        vic[7] = 2;
+        vic[8] = 600;
+
+        map.put("Xperia 10 IV", juan);
+        map.put("Xperia 10 III", vic);
+
+        String telefono = "Xperia 10 III";
+        cdi = map.get(telefono)[0];
+        pantallas = map.get(telefono)[1];
+        botones = map.get(telefono)[2];
+        pines = map.get(telefono)[3];
+        camaras = map.get(telefono)[4];
+        needPantalla = map.get(telefono)[5];
+        needPines = map.get(telefono)[6];
+        needCamaras = map.get(telefono)[7];
+        price = map.get(telefono)[8];
+        maxProd = 10 + cdi;
+
         // TODO code application logic here
         semProdScreens = new Semaphore(40);
         semConsScreens = new Semaphore(0);
         mutexScreens = new Semaphore(1);
-        
+
         semProdButt = new Semaphore(45);
         semConsButt = new Semaphore(0);
         mutexButt = new Semaphore(1);
-        
+
         semProdPins = new Semaphore(15);
         semConsPins = new Semaphore(0);
         mutexPins = new Semaphore(1);
-        
+
         semProdCams = new Semaphore(20);
         semConsCams = new Semaphore(0);
         mutexCams = new Semaphore(1);
-        
-        
-        
-        
+
     }
 
 }
--- src/Primitivas/Assembler.java
+++ src/Primitivas/Assembler.java
@@ -5,10 +5,106 @@
  */
 package Primitivas;
 
+import Main.Main;
+import java.util.concurrent.Semaphore;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author yunch
  */
-public class Assembler {
-    
+public class Assembler extends Thread {
+
+    float assTime;
+    Semaphore mutexScreens;
+    Semaphore mutexButts;
+    Semaphore mutexPins;
+    Semaphore mutexCams;
+
+    Semaphore mutexPhones;
+
+    Semaphore semConsScreens;
+    Semaphore semConsButts;
+    Semaphore semConsPins;
+    Semaphore semConsCams;
+
+    Semaphore semProdScreens;
+    Semaphore semProdButts;
+    Semaphore semProdPins;
+    Semaphore semProdCams;
+
+    boolean hired;
+
+    public Assembler(Semaphore mutexScreens,
+            Semaphore mutexButts,
+            Semaphore mutexPins,
+            Semaphore mutexCams,
+            Semaphore mutexPhones,
+            Semaphore semConsScreens,
+            Semaphore semConsButts,
+            Semaphore semConsPins,
+            Semaphore semConsCams,
+            Semaphore semProdScreens,
+            Semaphore semProdButts,
+            Semaphore semProdPins,
+            Semaphore semProdCams) {
+        this.assTime = (float) 2 * 1000 * 2;
+        this.mutexButts = mutexScreens;
+        this.mutexButts = mutexButts;
+        this.mutexPins = mutexPins;
+        this.mutexCams = mutexCams;
+        this.mutexPhones = mutexPhones;
+        this.semConsScreens = semConsScreens;
+        this.semConsButts = semConsButts;
+        this.semConsPins = semConsPins;
+        this.semConsCams = semConsCams;
+        this.semProdScreens = semProdScreens;
+        this.semProdButts = semProdButts;
+        this.semProdPins = semProdPins;
+        this.semProdCams = semProdCams;
+        this.hired = true;
+    }
+
+    public void setHired(boolean hired) {
+        this.hired = hired;
+    }
+
+    @Override
+    public void run() {
+        while (hired) {
+            try{
+                Thread.sleep((long)this.assTime);
+                this.semConsScreens.acquire((int)Main.needPantalla); // Nos aseguramos de que el consumidor (ensamblador) tenga las piezas necesarias 
+                this.semConsButts.acquire((int)Main.needPines);
+                this.semConsPins.acquire(1);
+                this.semConsCams.acquire((int)Main.needCamaras);
+                
+                    this.mutexScreens.acquire();
+                    this.mutexButts.acquire();
+                    this.mutexPins.acquire();
+                    this.mutexCams.acquire();
+                        Main.totalPhones++;
+                        Main.screens = Main.screens - 4;
+                        Main.buttons = Main.buttons - 40;
+                        Main.pins--;
+                        Main.cams = Main.cams -30; // Eliminamos de los almacenes las piezas consumidas
+                    this.mutexButts.release();
+                    this.mutexPins.release();
+                    this.mutexScreens.release();
+                    this.mutexPins.release();                    
+                    this.mutexCams.release();
+                    
+                this.semProdScreens.release((int)Main.needPantalla); // Se "sueltan" los permisos de semáforos de esas piezas
+                this.semProdButts.release((int)Main.needPines);
+                this.semProdPins.release(1);
+                this.semProdCams.release((int)Main.needCamaras);
+                
+            }
+            catch(InterruptedException e){
+                JOptionPane.showMessageDialog(null, "Ha ocurrido un error en la ejecución del ensamblador.", "ERROR", JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+            }
+            
+        }
+    }
 }
